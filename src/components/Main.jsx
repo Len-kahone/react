e8249f95ff902b2ee9e0035d2316d48956e72c14
Len-kahone <1030462746@qq.com>
2019-05-21 17:14:06 +0800

    实现单向绑定数据

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import List from '@/components/List'
 import cssobj from '@/css/main.scss'
-import bootcss from "bootstrap/dist/css/bootstrap.css"
+import bootcss from 'bootstrap/dist/css/bootstrap.css'
 var personList = [
   { name: '张三', id: 1, age: 18 },
   { name: '李四', id: 2, age: 28 },
@@ -9,21 +9,41 @@ var personList = [
   { name: '周杰伦', id: 4, age: 38 },
   { name: '张三丰', id: 5, age: 188 }
 ]
-console.log(cssobj);
+
 export default class Person extends React.Component {
   constructor() {
     super()
-    console.log(this)
-    this.state = {}
-  }
 
+    this.state = {}
+      this.state.personList = [...personList]
+      this.state.title = '人员列表'
+  }
+  tips=()=> {
+   
+    this.setState({title:"超级英雄"},()=>{
+      console.log(this.state.title)
+    })
+     
+  }
+  //监听输入框变化
+  textChange(){
+    console.log(this.refs.textRef.value)
+    const newTitle = this.refs.textRef.value
+    this.setState({
+      title:newTitle
+    })
+  }
   render() {
-    this.state.personList = [...personList]
+  
 
     return (
       <div>
-        <button className="btn btn-primary">按钮</button>
-        <h1 className={cssobj.title+" item"}>人员列表</h1>
+        <button onClick={() => this.tips()} className="btn btn-primary">
+          按钮
+        </button>
+        <br></br>
+        <input type="text"  value={this.state.title} ref="textRef" onChange={(e)=>this.textChange()}/> 
+        <h1 className={cssobj.title + ' item'}>{this.state.title}</h1>
         {this.state.personList.map(item => (
           <List key={item.id} {...item} />
         ))}
